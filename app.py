c65af435e0aaf59ff2a4f4d40cf9651603a04798
Andrés Pacheco <dpacheco@unab.edu.co>
2019-11-05 13:53:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def analyze_url():
 def about():
     return render_template('index.html')
 
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 8080))
 
 if __name__ == "__main__":
-	app.run(host='127.0.0.1', port=port, debug=True)
+	app.run(host='0.0.0.0, port=port, debug=True)
