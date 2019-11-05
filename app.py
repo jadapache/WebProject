8e1c58930e7986d5ba81c0b417186fb9f79560c5
Andrés Pacheco <dpacheco@unab.edu.co>
2019-11-05 14:17:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def analyze_url():
 def about():
     return render_template('index.html')
 
-port = int(os.environ.get('PORT', 8080))
+port = int(os.environ.get('PORT', 5000))
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0, port=port, debug=True)
+	app.run(host='0.0.0.0, port=port, debug=True) python3 app.py & #run application in background 
